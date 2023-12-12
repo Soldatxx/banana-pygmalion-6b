33f379bff68e05494d75a334bf5e028148c063bf
Soldatxx <47422725+Soldatxx@users.noreply.github.com>
2023-12-12 16:58:15 +0200

    Update download.py

--- download.py
+++ download.py
@@ -10,12 +10,12 @@ def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
     GPTJForCausalLM.from_pretrained(
-        "savvamadar/pygmalion-6b-v3-ggml-ggjt-q4_0", torch_dtype=torch.float16, low_cpu_mem_usage=True
+        "PygmalionAI/pygmalion-6b", torch_dtype=torch.float16, low_cpu_mem_usage=True
     )
     print("done")
 
     print("downloading tokenizer...")
-    GPT2Tokenizer.from_pretrained("savvamadar/pygmalion-6b-v3-ggml-ggjt-q4_0")
+    GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
     print("done")
 
 if __name__ == "__main__":
