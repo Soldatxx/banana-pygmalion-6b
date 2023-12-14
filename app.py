fbd5844d3a4b22e2ed485cf1cfef494754826414
Soldatxx <47422725+Soldatxx@users.noreply.github.com>
2023-12-14 13:58:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from transformers import GPTJForCausalLM, GPT2Tokenizer
 import torch
 
-device = "cuda:0"# if torch.cuda.is_available() else "cpu"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here.
@@ -10,7 +10,7 @@ def init():
     global tokenizer
 
     print("loading to CPU...")
-    model = GPTJForCausalLM.from_pretrained("PygmalionAI/pygmalion-6b", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model = GPTJForCausalLM.from_pretrained("PygmalionAI/pygmalion-2-13b", torch_dtype=torch.float16, low_cpu_mem_usage=True)
     print("done")
 
     # conditionally load to GPU
@@ -19,7 +19,7 @@ def init():
         model.cuda()
         print("done")
 
-    tokenizer = GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
+    tokenizer = GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-2-13b")
 
 
 # Inference is ran for every server call
